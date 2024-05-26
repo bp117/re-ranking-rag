846dcff2815e0cff964075ff87f7cbad97adedca
bp117 <bindupriya117@gmail.com>
2024-05-26 19:32:31 +0000

    reranking

---
+++ rag_101/samples.py
@@ -0,0 +1,15 @@
+query1 = "Where was the agriculture dataset collected for the USA?"
+query2 = "How many pdf data were collected from the USA?"
+query3 = "Which contries were used to collect dataset?"
+query4 = "What are the metrics used to evaluate the answers?"
+query5 = "how was the content and structure of available documents augmented?"
+query6 = "What was the answer generation process used in the paper?"
+
+queries = [
+    query1,
+    query2,
+    query3,
+    query4,
+    query5,
+    query6,
+]
